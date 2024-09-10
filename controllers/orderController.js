0fbd28132ade2e9eec4262e9b1fe04253d8965e9
Nishad <nishadalichenadan@gmail.com>
2024-09-10 11:40:18 +0530

    after razor pay 2  10-9-24

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -161,7 +161,7 @@ const placeOrder = async (req, res) => {
       console.log(options.amount+'--options');
       razorpay.orders.create(options, function (err, order) {
         console.log(order+'XP 6');
-        console.log('THIS IS THE ERROR --->'+err.error.description,err.error.,err.statusCode)
+        console.log('THIS IS THE ERROR --->'+err.error.description,err.error.reason,err.statusCode)
         return res.json({ order });
       });
       await decreaseProductQuantities(orderProducts);
